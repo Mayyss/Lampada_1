705e927fbcef01a4d3010d3657f695f42f7c98ea
MAYSSA MARTHA FRANCISCO DA SILVA <21193395@CFP127ALUNO.LOCAL>
2021-08-31 17:01:15 -0300

    Terminando a implantação de piscar

--- script.js
+++ script.js
@@ -3,3 +3,53 @@
 "use strict"
 //                                      evento
 const lampada = document.getElementById("lampada")
+let idLigar 
+let idDesligar
+
+
+function lampadaInteira(){
+    return lampada.src.includes ("ligada")
+}
+
+function ligarLampada(){
+    if(lampadaInteira())
+    {
+        lampada.src = "img/ligada.jpg"
+    }
+}
+
+function desligarLampada(){
+    if(lampadaInteira())
+    {
+        lampada.src = "img/desligada.jpg"
+    }
+}
+
+function quebrarLampada(){
+    lampada.src = "img/quebrada.jpg"
+}
+
+function piscar()
+{
+    const botaoPiscar = document.getElementById("btnPiscar")
+    if(botaoPiscar.textContent == "Parar"){
+        idLigar = setInterval(ligarLampada,500)
+        idDesligar = setInterval(desligarLampada,1000)
+        botaoPiscar.textContent = "Parar"
+    }
+}
+
+document.getElementById("btnLigar")
+    .addEventListener("click",ligarLampada)
+
+document.getElementById("btnDesligar")
+    .addEventListener("click",desligarLampada)
+
+document.getElementById("btnPiscar")
+    .addEventListener("click", piscar)
+
+lampada.addEventListener("mouseover", ligarLampada)
+lampada.addEventListener("mouseout",desligarLampada)
+
+
+document.getElementById("lampada").addEventListener("dblclick",quebrarLampada)
