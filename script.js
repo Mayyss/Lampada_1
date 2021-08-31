b1b6b609399a14f0495837141dcdd2089e7d6c9f
MAYSSA MARTHA FRANCISCO DA SILVA <21193395@CFP127ALUNO.LOCAL>
2021-08-31 14:43:39 -0300

    Adicionando imagens

---
+++ script.js
@@ -0,0 +1,5 @@
+// Fala pro navegador pra ser mais restrito, para prestar mais atenção aos erros
+// Para mostrar mais os erros
+"use strict"
+//                                      evento
+const lampada = document.getElementById("lampada")
